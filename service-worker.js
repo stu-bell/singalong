5280f096959d4c681084480f090df9ea4629ccc2
Stuart Bell <stu-b-doo@users.noreply.github.com>
2023-05-23 23:52:58 +0100

    trying to get offline to work

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,54 @@
 // cache name with version number
-const cacheName = 'lyrics-player-v1.6';
+const cacheName = 'lyrics-player-v1.9';
 
 // Files to cache
+// Add other files you want to cache
 const filesToCache = [
         '/',
+        '/manifest.json',
         '/index.html',
         '/assets/icon192.png',
         '/assets/icon512.png',
-        // 'styles.css',
-        // Add other files you want to cache
+        '/favicon.ico',
 ];
 
 // Installing Service Worker
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.log('Service Worker Install...')
   e.waitUntil((async () => {
     const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching');
     await cache.addAll(filesToCache);
+    console.log('Service Worker Installed')
   })());
 });
 
+// Check offline for cached resources when fetching
 self.addEventListener('fetch', function(event) {
+  console.log('Service Worker Fetch for', event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
     })
   );
 });
 
 // Clean up old caches (ie where the version name is different)
 self.addEventListener("activate", (e) => {
+  console.log('Service Worker Activate');
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key === cacheName) {
             return;
+          } else {
+            console.log('Service Worker Deleting Cache', key);
+            return caches.delete(key);
           }
-          return caches.delete(key);
         })
       );
     })
