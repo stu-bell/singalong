2b4916127ae60e2c06db7690975a17a22ab592c0
Stuart Bell <stu-b-doo@users.noreply.github.com>
2023-05-23 14:59:52 +0100

    retry caching

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,46 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
+// cache name with version number
+const cacheName = 'lyrics-player-v1';
+
+// Files to cache
+const filesToCache = [
         '/',
-        'index.html',
+        '/index.html',
+        '/assets/icon192.png',
+        '/assets/icon512.png',
         // 'styles.css',
         // Add other files you want to cache
-      ]);
-    })
-  );
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Clean up old caches (ie where the version name is different)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
     })
   );
 });
