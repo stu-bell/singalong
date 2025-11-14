8bd523630bfd7afd6a773eacfc0908dfb4757a24
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2025-11-14 14:56:39 +0000

    try full screen request

--- src/util.ts
+++ src/util.ts
@@ -15,29 +15,45 @@ const propOrDefault =
     obj.hasOwnProperty(propName) ? obj[propName] : defVal;
 
 const isFullScreen = () => !!( // doesn't work if F11 is used
+    // @ts-ignore
     document.fullscreenElement ||
+    // @ts-ignore
     document.webkitFullscreenElement ||
+    // @ts-ignore
     document.mozFullScreenElement ||
+    // @ts-ignore
     document.msFullscreenElement   );
 
 const requestFullScreen = (el: HTMLElement = document.documentElement) => {
-  console.log ('full screen requested on ', el)
+  alert('full screen requested')
   // will only work from a secure context, eg in a handler from a user triggered event, such as click
+    // @ts-ignore
   if (el.requestFullscreen) {
+    // @ts-ignore
       el.requestFullscreen();
+    // @ts-ignore
   } else if (el.webkitRequestFullscreen) { // Safari
+    // @ts-ignore
       el.webkitRequestFullscreen();
+    // @ts-ignore
   } else if (el.msRequestFullscreen) { // old Edge
+    // @ts-ignore
       el.msRequestFullscreen();
   }
 }
 
 const exitFullScreen = () => {
+    // @ts-ignore
   if (document.exitFullscreen) {
+    // @ts-ignore
     document.exitFullscreen();
+    // @ts-ignore
   } else if (document.webkitExitFullscreen) {
+    // @ts-ignore
     document.webkitExitFullscreen();
+    // @ts-ignore
   } else if (document.msExitFullscreen) {
+    // @ts-ignore
     document.msExitFullscreen();
   }
 }
